76e91b075832d1709c001b1c2e228035051ca3ab
Leesowon <66356241+Leesowon@users.noreply.github.com>
2023-02-22 01:10:17 +0900

    Add files via upload

---
+++ 5.6.3 softmax-with-loss.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def cross_entropy_error(y,t):
+    delta = 1e-7
+    return -np.sum(t*np.log(y+delta))
+
+# 동일한 계산
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+
+cee = cross_entropy_error(np.array(y), np.array(t))
+print(cee)  # 0.510825457099
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+
+cee = cross_entropy_error(np.array(y), np.array(t))
+print(cee)  # 2.30258409299
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 손실함수
+        self.y = None    # softmax의 출력
+        self.t = None    # 정답 레이블(원-핫 인코딩 형태)
+        
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size: # 정답 레이블이 원-핫 인코딩 형태일 때
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+        
+        return dx
